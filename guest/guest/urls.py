cedc94e19038d1668b416f4fa55a463eaa14788b
Eric <52667243@qq.com>
2020-04-06 18:25:55 +0800

    2

--- guest/guest/urls.py
+++ guest/guest/urls.py
@@ -19,9 +19,15 @@ from sign import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^index/', views.index),
     url(r'^$', views.index),
+    url(r'^index/$', views.index),
     url(r'^accounts/login/', views.index),    
-    url(r'^login_action/', views.login_action),
-    url(r'^event_manage/', views.event_manage),
+    url(r'^login_action/$', views.login_action),
+    url(r'^event_manage/$', views.event_manage),
+    url(r'^search_name/$',views.search_name),
+    url(r'^guest_manage/$',views.guest_manage),
+    url(r'^sign_index/(?P<eid>[0-9]+)/$',views.sign_index),
+    url(r'^sign_index_action/(?P<eid>[0-9]+)/$',views.sign_index_action),
+    url(r'^logout/$',views.logout)
+
 ]
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from sign.models import Event,Guest
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
 def index(request):
@@ -13,6 +15,7 @@ def login_action(request):
 		password = request.POST.get('password')
 		user = auth.authenticate(username=username,password=password)
 		if user is not None:
+			auth.login(request,user)
 			response = HttpResponseRedirect('/event_manage/')
 			# response.set_cookie('user',username,3600)
 			request.session['user'] = username
@@ -25,6 +28,68 @@ def login_action(request):
 
 @login_required
 def event_manage(request):
-	#username = request.COOKIE.get('user')
+	event_list = Event.objects.all()
 	username = request.session.get('user')
-	return render(request,'event_manage.html',{"user":username})
+	return render(request,'event_manage.html',
+		{"user":username,"events":event_list})
+
+@login_required
+def search_name(request):
+	username = request.session.get('user')
+	search_name = request.GET.get('name')
+	event_list = Event.objects.filter(name__contains=search_name)
+	return render(request,'event_manage.html',
+				{"user":username,"events":event_list})
+
+@login_required
+def guest_manage(request):
+	username = request.session.get('user')
+	guest_list = Guest.objects.all()
+	paginator = Paginator(guest_list,3)
+	page = request.GET.get('page')
+	try:
+		contacts = paginator.page(page)
+	except PageNotAnInteger:
+		contacts = paginator.page(1)
+	except Emptypage:
+		contacts = paginator.page(paginator.num_pages)
+	return render(request,"guest_manage.html",
+				{"user":username,"guests":contacts})
+
+@login_required
+def sign_index(request,eid):
+	event = get_object_or_404(Event,id = eid)
+	return render(request,'sign_index.html',{"event":event})
+
+@login_required
+def sign_index_action(request,eid):
+	event = get_object_or_404(Event,id = eid)
+	phone  = request.POST.get('phone')
+	print(phone)
+	result = Guest.objects.filter(phone=phone)
+	if not result:
+		return render(request,'sign_index.html',
+			{'event':event,'hint':'phone error.',
+			'account':f'all number account:{len(Guest.objects.all())}',
+			'nums':f'now sign in number:{len(Guest.objects.filter(sign=True))},\
+			not sign member:{Guest.objects.filter(sign=False)}'})
+	result = Guest.objects.get(phone=phone,event_id=eid)
+	if result.sign:
+		return render(request,'sign_index.html',
+			{'event':event,'hint':'user has sign in',
+			'account':f'all number account:{len(Guest.objects.all())}',
+			'nums':f'now sign in number:{len(Guest.objects.filter(sign=True))},\
+			not sign member:{Guest.objects.filter(sign=False)}'})
+	else:
+		Guest.objects.filter(phone=phone,event_id=eid).update(sign='1')
+		return render(request,'sign_index.html',
+			{'event':event,'hint':'sign in success!','guest':result,
+			'account':f'all number account:{len(Guest.objects.all())}',
+			'nums':f'now sign in number:{len(Guest.objects.filter(sign=True))},\
+			not sign member:{Guest.objects.filter(sign=False)}'})
+
+@login_required
+def logout(request):
+	auth.logout(request)
+	response = HttpResponseRedirect('/index/')
+	return response
