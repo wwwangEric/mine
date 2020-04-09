5bedc4a27323bc0a63071e4e3c141f96395b878d
Eric <52667243@qq.com>
2020-04-09 22:23:22 +0800

    0409 2223

--- guest/guest/urls.py
+++ guest/guest/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
-from sign import views
+from sign import views,urls
+import sign
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -28,6 +29,7 @@ urlpatterns = [
     url(r'^guest_manage/$',views.guest_manage),
     url(r'^sign_index/(?P<eid>[0-9]+)/$',views.sign_index),
     url(r'^sign_index_action/(?P<eid>[0-9]+)/$',views.sign_index_action),
-    url(r'^logout/$',views.logout)
+    url(r'^logout/$',views.logout),
+    url(r'^api/',include(sign.urls, namespace="sign")),
 
 ]
